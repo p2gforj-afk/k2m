6f58a1322f8a24d3d6e5b84afdcb98ed8ff995d7
chaigne <rchaigne@enssat.fr>
2025-12-24 13:23:48 +0100

    update_avl

--- AVL.c
+++ AVL.c
@@ -13,21 +13,12 @@ struct sdd_synonymes{
 
 };
 
-typedef struct sdd_dico * T_dico;
-struct sdd_dico{
-    char * mot; 
-    int eq;
-    T_syn s;
-    T_dico l; //sag
-    T_dico r; //sad
-};
-
 void S_init_vide(T_syn * p){
     *p = NULL;
 }
 
 bool appartient_a (T_syn p, char * mot){
-    if(p != NULL){
+    if(p == NULL){
         return false;
     }
     int cmp = strcmp(p->cur, mot);
@@ -46,31 +37,26 @@ int hauteur(T_syn p){
     if (p == NULL){
         return -1;
     }
-    return(1 + max(hauteur(p->l),hauteur(p->r)));
+    return(hauteur(p->l) > hauteur(p->r)) ? 1 + hauteur(p->l)
+                                          : 1 + hauteur(p->r);
 }
 
 void equilibre(T_syn * p){
-    if (*p == NULL){
-        (*p)->eq = 0;
-    }
-    else {    
+    if (*p != NULL) 
         (*p)->eq = (hauteur((*p)->l) - hauteur((*p)->r));
-    }
 }
 
 void Rr(T_syn * p){
-    T_syn * aux;
-    (*aux) = (*p)->l;
-    (*p)->l = (*aux)->r;
-    (*aux)->r = *p;
-    *p = *aux;
+    T_syn  aux = (*p)->l;
+    (*p)->l = aux->r;
+    aux->r = *p;
+    *p = aux;
 }
 void Lr(T_syn * p){
-    T_syn * aux;
-    (*aux) = (*p)->r;
-    (*p)->r = (*aux)->l;
-    (*aux)->l = *p;
-    *p = *aux;
+    T_syn aux = (*p)->r;
+    (*p)->r = aux->l;
+    aux->l = *p;
+    *p = aux;
 }
 void LRr(T_syn *p){
     Lr(&(*p)->l);
@@ -85,6 +71,7 @@ void RLr(T_syn *p){
 void ajout_synonyme (T_syn * p, char * mot){
     if(!appartient_a(*p,mot)){
         if (*p == NULL){
+            *p = malloc(sizeof(**p));
             (*p)->cur = mot;
             (*p)->r = NULL;
             (*p)->l = NULL;
@@ -115,20 +102,20 @@ void ajout_synonyme (T_syn * p, char * mot){
             new->cur = mot;
             new->l = NULL;
             new->r = NULL;
-            if(menma = 1){
+            if(menma == 1){
                 l->l = new;
             }
             else{
                 l->r = new;
             }
-            T_syn l2 = *p;
+            T_syn l2;
             T_syn elec = *p;
             while(elec != NULL){
                 l2 = *p;
-                elec = NULL; 
+                elec = NULL;
                 while(l2 != new){
                     equilibre(&l2);
-                    if(l2->eq > 1 || l2->eq <1){
+                    if(l2->eq > 1 || l2->eq < -1){
                         elec = l2;
                     }
                     if (strcmp(l2->cur, mot) < 0){
@@ -139,29 +126,33 @@ void ajout_synonyme (T_syn * p, char * mot){
                     }
                 }
                 if(elec != NULL){
-                    if (elec->eq == 2){
-                        if(hauteur(elec->r) > hauteur(elec->l)){
-                            LRr(&elec);
-                        }
-                        else{
-                            Rr(&elec);
-                        }
-                    }
-                    else{
-                        if(hauteur(elec->l) > hauteur(elec->r)){
-                            RLr(&elec);
-                        }
-                        else{
-                            Lr(&elec);
-                        }
-                    }
+                    if (elec->eq == 2)
+                        (hauteur(elec->r) > hauteur(elec->l))
+                            ? LRr(&elec)
+                            : Rr(&elec);
+                    else
+                        (hauteur(elec->l) > hauteur(elec->r))
+                            ? RLr(&elec)
+                            : Lr(&elec);
                 }
             }
         }
     }
 }
 
+typedef struct sdd_dico * T_dico;
+struct sdd_dico{
+    char * mot; 
+    int eq;
+    T_syn s;
+    T_dico l; //sag
+    T_dico r; //sad
+};
+
 void D_init_vide(T_dico *p){
         *p = NULL;
 }
 
+int main(void){
+    return 0;
+}
