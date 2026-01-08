255909692eda8896c67c9e52fbc8cc8fa2038073
chaigne <rchaigne@enssat.fr>
2026-01-08 11:31:48 +0100

    fancy_hachage

--- hachage.c
+++ hachage.c
@@ -6,6 +6,7 @@
 
 #include "liste.h"
 
+#define mod 9
 
 typedef struct hachage T_ht;
 struct hachage{
@@ -17,21 +18,21 @@ void create_ht(T_ht * pht, int k){
     pht->n = k;
     pht->l = malloc(k*sizeof(T_liste ));
 }
-void init(T_ht * ht, int k){
-    for(int i = 0; i<k; i++){
+void init(T_ht * ht){
+    for(int i = 0; i<ht->n; i++){
         ht->l[i] = NULL;
     }
 }
-void insertion(T_ht * ht, int k, int cle){
-    int m = cle%k;
+void insertion(T_ht * ht, int cle){
+    int m = cle%ht->n;
     T_liste new = malloc(sizeof(*new));
     new->elt = cle;
     new->svt = ht->l[m];
     ht->l[m] = new;
 }
 
-bool recherche(T_ht * ht, unsigned int k, unsigned int cle){
-    int m = cle%k;
+bool recherche(T_ht * ht, unsigned int cle){
+    int m = cle%ht->n;
     T_liste  p = ht->l[m];
     while (p != NULL){
         if (p->elt == cle)
@@ -43,7 +44,35 @@ bool recherche(T_ht * ht, unsigned int k, unsigned int cle){
 
 int main(void){
     T_ht TABLE;
-    create_ht(&TABLE, 9);
-    init(&TABLE, 9);
+    create_ht(&TABLE, mod);
+    init(&TABLE);
+
+    insertion(&TABLE, 10);
+        insertion(&TABLE, 17);
+            insertion(&TABLE, 12);
+                insertion(&TABLE, 30);
+                    insertion(&TABLE, 20);
+                        insertion(&TABLE, 15);
+                            insertion(&TABLE, 19);
+                                insertion(&TABLE, 28);
+                                    insertion(&TABLE, 5);
+
+
+    if (recherche(&TABLE, 17))
+        printf("ca");
+    if (!recherche(&TABLE, 42))
+        printf(" marche \n");
+    
+    for (int i = 0; i<mod; i++){
+        printf("%d. ",i);
+        T_liste p = TABLE.l[i];
+        while (p!=NULL){
+            printf("%d",p->elt);
+            if (p->svt != NULL)
+                printf(" -> ");
+            p = p->svt;
+        }
+        printf("\n");
+    }
     return 0;
 }
