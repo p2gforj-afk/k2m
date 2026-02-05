7ec7135f6e7f555fd2c25e574090f5cb442a6014
chaigne <rchaigne@enssat.fr>
2026-02-05 18:13:38 +0100

    <err.h>gate

--- sort-make/build/CMakeFiles/extra_clean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/extra_clean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/extra_clean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
