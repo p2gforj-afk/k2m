2dce264d4d9f6c323c734e2047b59cd7b33788f6
ムカ <rchaigne@emssat.fr>
2026-04-07 13:45:44 +0200

    oe

--- dev-web/seance4/main.js
+++ dev-web/seance4/main.js
@@ -1,260 +1,194 @@
-window.addEventListener("DOMContentLoaded", init);
-
-// Rock >  Scissors > Paper > Rock > ...
-const choices = ["rock", "scissors", "paper"];
-let humanChoice; //0 for Rock,1 for Scissors or 2 for Paper
-let computerChoice; // 0 for Rock,1 for Scissors or 2 for Paper
-
-let humanScore = 0;
-let computerScore = 0;
-
-let gameLoop;
-let cptGame = 0; // 3 sets for a game
-let cptLoop = 0; // human can choose for 5 seconds, computer doesn't see the human's choice and play. Score is set within 2 seconds
-
-//*********************
-// HTML elements
-let humanScoreLabel; //where to print the human's score
-let computerScoreLabel; // guess :)
-
-let ctxHuman; // the 2d context where to draw the human's choice
-let ctxComputer;
-
-let rockBtn; // the button with witch the human chooses a rock
-let paperBtn; // ... a paper
-let scissorsBtn; // ... a scissors
-//*********************
-
-//*******
-// pre-loaded images to be drawn in canvases
-var rockImage;
-var paperImage;
-var scissorsImage;
-//*******
-
-function init() {
-  //*************
-  // This parts sets values for HTML elements
-  // >>>>>> retreive 2d contexts
-  ctxHuman = document.getElementById("humanPlayerCanvas").getContext("2d");
-  ctxComputer = document
-    .getElementById("computerPlayerCanvas")
-    .getContext("2d");
-  // >>>>>> retreive score labels
-  humanScoreLabel = document.getElementById("humanScoreLabel");
-  computerScoreLabel = document.getElementById("computerScoreLabel");
-  // >>>>>> retreive buttons
-  rockBtn = document.getElementById("rockBtn");
-  paperBtn = document.getElementById("paperBtn");
-  scissorsBtn = document.getElementById("scissorsBtn");
-  //*************
-
-  //************************************************************
-  // in this part, we wait for all the images to load before starting the game
-  let rockImageOk = false;
-  let paperImageOk = false;
-  let scissorsImageOk = false;
-
-  rockImage = new Image(); // create an image object
-  rockImage.src = "./rock.png"; // set its source, may take some time
-  paperImage = new Image();
-  paperImage.src = "./paper.png";
-  scissorsImage = new Image();
-  scissorsImage.src = "./scissors.png";
-
-  // >>>>>>>>>>>>> begin of callBacks
-  function rockLoaded() {
-    console.log("Rock Loaded");
-    rockImageOk = true;
-    fireEvent();
-  }
-  function paperLoaded() {
-    console.log("Paper Loaded");
-    paperImageOk = true;
-    fireEvent();
-  }
-  function scissorsLoaded() {
-    console.log("Scissors Loaded");
-    scissorsImageOk = true;
-    fireEvent();
-  }
-  function launchGame() {
-    gameLoop = setInterval(playGame, 1000);
-  }
-  // >>>>>>>>>>>>> end of callBacks
-
-  // >>>>>>>>>>>>> Link events and callBacks
-  // Link the "load" event fired by the "rockImage" object with the "rockLoaded" callback
-  rockImage.addEventListener("load", rockLoaded);
-  // Link the "load" event fired by the "paperImage" object with the "paperLoaded" callback
-  paperImage.addEventListener("load", paperLoaded);
-  // Link the "load" event fired by the "scissorsImage" object with the "scissorsLoaded" callback
-  scissorsImage.addEventListener("load", scissorsLoaded);
-  // Link the "allImagesOk" event fired by the "window" object with the "launchGame" callback
-  window.addEventListener("allImagesOk", launchGame);
-  // Link the "click" event fired by the "rockBtn" object with a call to "humanSelects("rock")"
-  rockBtn.addEventListener("click", helper1);
-  // Link the "click" event fired by the "paperBtn" object with a call to "humanSelects("paper")"
-  paperBtn.addEventListener("click", function () {
-    humanSelects("paper");
-  });
-  // Link the "click" event fired by the "scissorsBtn" object with a call to "humanSelects("scissors")"
-  scissorsBtn.addEventListener("click", function () {
-    humanSelects("scissors");
-  });
-  // >>>>>>>>>>>>>
-
-  function helper1() {
-    humanSelects("rock");
+class ManagedImage {
+  constructor(src) {
+    this.src = src;
+    this.image = new Image();
+    this.loaded = false;
   }
 
-  var helper2 = function () {
-    //.....
-  };
-
-  // this function fires a custom event "allImagesOk" when all images are loaded
-  function fireEvent() {
-    if (rockImageOk & scissorsImageOk & paperImageOk) {
-      window.dispatchEvent(new Event("allImagesOk"));
-    }
-  }
-}
-
-function playGame() {
-  cptLoop = cptLoop + 1;
-  if (cptLoop == 5) {
-    disableButtons();
-    computerChoice = choices[Math.floor(Math.random() * 3)];
-    computerSelects(computerChoice);
-    setScore();
+  load() {
+    return new Promise((resolve, reject) => {
+      this.image.src = this.src;
+      this.image.onload = () => {
+        this.loaded = true;
+        resolve(this);
+      };
+      this.image.onerror = () => reject(`image not loaded`);
+    });
   }
 
-  if (cptLoop == 7) {
-    cptLoop = 0;
-    resetGame();
+  getImage() {
+    return this.image;
   }
-}
-
-function setScore() {
-  if (humanChoice == undefined) {
-    computerScore++;
-  } else {
-    var val = computerChoice - humanChoice;
-    console.log(
-      "computer plays " +
-        choices[computerChoice] +
-        " and human plays " +
-        choices[humanChoice],
-    );
-    if (val == 1 || val == -2) {
-      humanScore++;
-      console.log("    human wins");
-    }
-    if (val == -1 || val == 2) {
-      computerScore++;
-      console.log("    computer wins");
-    }
-  }
-  humanScoreLabel.innerHTML = humanScore;
-  computerScoreLabel.innerHTML = computerScore;
-}
-
-function resetGame() {
-  cptGame++;
-  ctxComputer.clearRect(0, 0, 328, 328);
-  ctxHuman.clearRect(0, 0, 328, 328);
-  computerChoice = undefined;
-  humanChoice = undefined;
-  if (cptGame == 3) {
-    clearInterval(gameLoop); // End Gane
-  } else {
-    ableButtons();
-  }
-}
-
-// this function sets the "disabled" property of buttons to "false" : human can click on any of them
-function ableButtons() {
-  rockBtn.disabled = false;
-  paperBtn.disabled = false;
-  scissorsBtn.disabled = false;
-}
-
-// this function sets the "disabled" property of buttons to "true": human cannot click on any of them
-function disableButtons() {
-  rockBtn.disabled = true;
-  paperBtn.disabled = true;
-  scissorsBtn.disabled = true;
-}
-
-function humanSelects(shifumi) {
-  let image;
-  switch (shifumi) {
-    case "rock":
-      image = rockImage;
-      humanChoice = 0;
-      break;
-    case "scissors":
-      image = scissorsImage;
-      humanChoice = 1;
-      break;
-    case "paper":
-      image = paperImage;
-      humanChoice = 2;
-      break;
-    default:
-      console.log("not a shifumi!");
-  }
-  ctxHuman.drawImage(image, 0, 0, 328, 328);
-  disableButtons();
-}
-function computerSelects(shifumi) {
-  let image;
-  switch (shifumi) {
-    case "rock":
-      image = rockImage;
-      computerChoice = 0;
-      break;
-    case "scissors":
-      image = scissorsImage;
-      computerChoice = 1;
-      break;
-    case "paper":
-      image = paperImage;
-      computerChoice = 2;
-      break;
-    default:
-      console.log("not a shifumi!");
-  }
-  ctxComputer.drawImage(image, 0, 0, 328, 328);
 }
 
 class ImageManager {
-  constructor({ name, src }, imageLoader) {
-    this.name = name;
-    this.src = src;
-    this.image = new Image();
-    this.image.addEventListener("load", (e) => {
-      imageLoader.dispatchEvent(
-        new CustomEvent(`imageLoaded`, { detail: this.name }),
-      );
+  constructor() {
+    this.managedImages = {};
+  }
+
+  async loadImages(imageSources) {
+    const promises = Object.entries(imageSources).map(([name, src]) => {
+      const mImg = new ManagedImage(src);
+      this.managedImages[name] = mImg;
+      return mImg.load();
     });
+    return Promise.all(promises);
   }
-  loadThisImage() {
-    this.image.src = this.src;
+
+  getImage(choice) {
+    return this.managedImages[choice]?.getImage();
   }
 }
 
-const myArray = [
-  { name: "rock", src: "./assets/rock.png" },
-  { name: "paper", src: "./assets/paper.png" },
-  { name: "scissors", src: "./assets/scissors.png" },
-];
-const myImagesLoader = new ImagesLoader(myArray);
+class UI {
+  constructor(canvasSize) {
+    this.canvasSize = canvasSize;
+  }
 
-window.addEventListener("allImagesOk", launchGame);
-function launchGame() {
-  rockImage = myImagesLoader.getImage("rock");
-  paperImage = myImagesLoader.getImage("paper");
-  scissorsImage = myImagesLoader.getImage("scissors");
-  gameLoop = setInterval(playGame, 1000);
+  initializeElements() {
+    this.humanScoreLabel = document.getElementById("humanScoreLabel");
+    this.computerScoreLabel = document.getElementById("computerScoreLabel");
+    this.ctxHuman = document.getElementById("humanPlayerCanvas").getContext("2d");
+    this.ctxComputer = document.getElementById("computerPlayerCanvas").getContext("2d");
+    this.rockBtn = document.getElementById("rockBtn");
+    this.paperBtn = document.getElementById("paperBtn");
+    this.scissorsBtn = document.getElementById("scissorsBtn");
+  }
+
+  setupEventListeners(game) {
+    this.rockBtn.addEventListener("click", () => game.humanPlaysChoice("rock"));
+    this.paperBtn.addEventListener("click", () => game.humanPlaysChoice("paper"));
+    this.scissorsBtn.addEventListener("click", () => game.humanPlaysChoice("scissors"));
+  }
+
+  drawHumanChoice(choice, imageManager) {
+    const img = imageManager.getImage(choice);
+    this.ctxHuman.drawImage(img, 0, 0, this.canvasSize, this.canvasSize);
+  }
+
+  drawComputerChoice(choice, imageManager) {
+    const img = imageManager.getImage(choice);
+    this.ctxComputer.drawImage(img, 0, 0, this.canvasSize, this.canvasSize);
+  }
+
+  enableButtons() {
+    this.rockBtn.disabled = false;
+    this.paperBtn.disabled = false;
+    this.scissorsBtn.disabled = false;
+  }
+
+  disableButtons() {
+    this.rockBtn.disabled = true;
+    this.paperBtn.disabled = true;
+    this.scissorsBtn.disabled = true;
+  }
+  updateScoreDisplay(hScore, cScore) {
+    this.humanScoreLabel.textContent = hScore;
+    this.computerScoreLabel.textContent = cScore;
+  }
+
+  clearCanvases() {
+    this.ctxHuman.clearRect(0, 0, this.canvasSize, this.canvasSize);
+    this.ctxComputer.clearRect(0, 0, this.canvasSize, this.canvasSize);
+  }
+
+  showGameOver(hScore, cScore) {
+    if (hScore > cScore) {
+      alert("human wins, well played")
+    }
+    else {
+      alert("computer wins, try again")
+    }
+  }
 }
+
+class Game {
+  constructor() {
+    this.CHOICES = ["rock", "scissors", "paper"];
+    this.CANVAS_SIZE = 328;
+    this.humanScore = 0;
+    this.computerScore = 0;
+    this.cptLoop = 0; // timer for each round
+  }
+
+  setDependencies(imageManager, ui) {
+    this.imageManager = imageManager;
+    this.ui = ui;
+  }
+
+  startGame() {
+    this.ui.initializeElements();
+    this.ui.setupEventListeners(this);
+    this.gameLoop = setInterval(() => this.playRound(), 1000);
+  }
+
+  stopGame() {
+    clearInterval(this.gameLoop);
+    this.ui.showGameOver(this.humanScore, this.computerScore);
+  }
+
+  playRound() {
+    this.cptLoop++;
+    if (this.cptLoop === 5) {
+      this.ui.disableButtons();
+      const randomIndex = Math.floor(Math.random() * 3);
+      this.computerChoice = randomIndex;
+      this.ui.drawComputerChoice(this.CHOICES[randomIndex], this.imageManager);
+      this.calculateScore();
+    }
+
+    if (this.cptLoop === 7) {
+      this.cptLoop = 0;
+      this.resetRound();
+    }
+  }
+
+  calculateScore() {
+    if (this.humanChoice === undefined) {
+      this.computerScore++;
+    } else {
+      const val = this.computerChoice - this.humanChoice;
+      if (val === 1 || val === -2) this.humanScore++;
+      else if (val === -1 || val === 2) this.computerScore++;
+    }
+    this.ui.updateScoreDisplay(this.humanScore, this.computerScore);
+  }
+
+  resetRound() {
+    this.ui.clearCanvases();
+    this.humanChoice = undefined;
+    this.computerChoice = undefined;
+
+    if (this.humanScore == 3 || this.computerScore == 3) {
+      this.stopGame();
+    } else {
+      this.ui.enableButtons();
+    }
+  }
+
+  humanPlaysChoice(choice) {
+    this.humanChoice = this.getChoiceIndex(choice);
+    this.ui.drawHumanChoice(choice, this.imageManager);
+    this.ui.disableButtons();
+  }
+
+  getChoiceIndex(choice) {
+    return this.CHOICES.indexOf(choice);
+  }
+}
+
+// launch the game
+const game = new Game();
+const ui = new UI(game.CANVAS_SIZE);
+const imgMgr = new ImageManager();
+
+const sources = {
+  rock: "./assets/rock.png",
+  paper: "./assets/paper.png",
+  scissors: "./assets/scissors.png"
+};
+
+imgMgr.loadImages(sources).then(() => {
+  game.setDependencies(imgMgr, ui);
+  game.startGame();
+});
--- system/tp4/exo4_a.c
+++ system/tp4/exo4_a.c
@@ -1,25 +1,33 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-int main(){
+int main() {
 
-  int tub_nomme; 
+  int tub_nomme;
 
-  if (mkfifo("ma_pipe", 0600) ==-1){printf("tube existant : on va travailler avec le tube existant\n");} 
-  else {printf("tube cree dans le processus ecrivain\n");}
+  if (mkfifo("ma_pipe", 0600) == -1) {
+    printf("tube existant : on va travailler avec le tube existant\n");
+  } else {
+    printf("tube cree dans le processus ecrivain\n");
+  }
 
   /* Ouverture du tube pour ecriture */
   tub_nomme = open("ma_pipe", O_WRONLY);
-  if(tub_nomme == -1){printf("ouverture tube impossible dans le processus ecrivain\n");return EXIT_FAILURE;}
+  if (tub_nomme == -1) {
+    printf("ouverture tube impossible dans le processus ecrivain\n");
+    return EXIT_FAILURE;
+  }
 
   /* Ecriture dans le tube */
-  printf("ecriture 1\n"); write(tub_nomme, "Tous les matins il achetait ", 28);
+  printf("ecriture 1\n");
+  write(tub_nomme, "Tous les matins il achetait ", 28);
   sleep(10);
-  printf("ecriture 2\n"); write(tub_nomme, "son petit pain au chocolat la la la la", 38);
+  printf("ecriture 2\n");
+  write(tub_nomme, "son petit pain au chocolat la la la la", 38);
 
   /* Fermeture du tube */
   close(tub_nomme);
