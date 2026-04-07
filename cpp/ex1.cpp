bd86f4cb46e90d84190632042fed7e22ed57b8d6
ムカ <rchaigne@emssat.fr>
2026-04-08 00:00:01 +0200

    nvim >>> vscode

--- cpp/ex1.cpp
+++ cpp/ex1.cpp
@@ -1,61 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <iostream>
 #include <numeric>
+#include <vector>
 
-template <typename T>
-class Statistiques
-{
+template <typename T> class Statistiques {
 private:
-    std::vector<T> donnees;
+  std::vector<T> donnees;
 
 public:
-    void ajouter(T valeur);
-    void trie();
-        T obtenirMax() const;
-    double obtenirMoyenne() const;
+  void ajouter(T valeur);
+  void trie();
+  T obtenirMax() const;
+  double obtenirMoyenne() const;
 };
 
-template <typename T>
-void Statistiques<T>::ajouter(T valeur)
-{
-    donnees.push_back(valeur);
+template <typename T> void Statistiques<T>::ajouter(T valeur) {
+  donnees.push_back(valeur);
 }
 
-template <typename T>
-T Statistiques<T>::obtenirMax() const
-{
-    if (donnees.empty())
-        return 0;
-    return *std::max_element(donnees.begin(), donnees.end());
+template <typename T> T Statistiques<T>::obtenirMax() const {
+  if (donnees.empty())
+    return 0;
+  return *std::max_element(donnees.begin(), donnees.end());
 }
 
-template <typename T>
-double Statistiques<T>::obtenirMoyenne() const
-{
-    if (donnees.empty())
-        return 0.0;
-    T somme = std::accumulate(donnees.begin(), donnees.end(), T(0.0));
-    return static_cast<double>(somme) / donnees.size();
+template <typename T> double Statistiques<T>::obtenirMoyenne() const {
+  if (donnees.empty())
+    return 0.0;
+  T somme = std::accumulate(donnees.begin(), donnees.end(), T(0.0));
+  return static_cast<double>(somme) / donnees.size();
 }
 
-template <typename T>
-void Statistiques<T>::trie()
-{
-    std::sort(donnees.begin(), donnees.end());
+template <typename T> void Statistiques<T>::trie() {
+  std::sort(donnees.begin(), donnees.end());
 }
 
-int main()
-{
-    Statistiques<double> notes;
-    notes.ajouter(12.5);
-    notes.ajouter(18.0);
-    notes.ajouter(7.5);
-    notes.ajouter(14.0);
-    notes.ajouter(11.0);
-    notes.ajouter(19.5);
-    notes.ajouter(5.0);
-    notes.trie();
-    std::cout << "notes max: " << notes.obtenirMax() << std::endl;
+int main() {
+  Statistiques<double> notes;
+  notes.ajouter(12.5);
+  notes.ajouter(18.0);
+  notes.ajouter(7.5);
+  notes.ajouter(14.0);
+  notes.ajouter(11.0);
+  notes.ajouter(19.5);
+  notes.ajouter(5.0);
+  notes.trie();
+  std::cout << "notes max: " << notes.obtenirMax() << std::endl;
 }
